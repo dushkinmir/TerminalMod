21b92842755115b9c232f495d22bb07a987d7d82
trywyf <95691523+trywyf@users.noreply.github.com>
2023-04-03 14:19:11 +0900

    remove ext/eval

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from base.module import BaseModule
 
 # Extensions
-from .extensions.eval import EvalCmd
 from .extensions.term import TermCmd
 
 class TerminalMod(BaseModule):
     @property
     def module_extensions(self):
         return [
-            TermCmd,
-            EvalCmd
+            TermCmd
         ]
